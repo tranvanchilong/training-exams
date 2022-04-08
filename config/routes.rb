1883fbbaaee30f87d7bc9ba1dacde3401782be02
tranvanchilong <tranvanchilong@gmail.com>
2022-04-08 09:22:28 +0700

    demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'users#index'
+   root 'users#index'
 
-  # Example of regular route:
+  # Example of regular route:s
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
