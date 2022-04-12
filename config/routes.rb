e2081f41b8a0a2640b23ec0801f44a27904a9668
minhpham <minhpham161895@gmail.com>
2022-04-12 13:38:56 +0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     resources :password_resets, only:[:new,:create,:edit,:update]
   end
 <<<<<<< HEAD
+<<<<<<< HEAD
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -74,6 +75,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-=======
->>>>>>> profile
 end
