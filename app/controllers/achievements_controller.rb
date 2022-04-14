9f0474530cc6a95d814f7cd6bab431c712e8d04f
tranvanchilong <tranvanchilong@gmail.com>
2022-04-14 16:55:25 +0700

    profile_achiviement

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,12 @@
 class AchievementsController < ApplicationController
   def index
     @user = User.find params[:user_id]
-    @user_exams = @user.user_exams.includes(:exam) # fix N+1
+    if @user
+      @user_exams = @user.user_exams.includes(:exam) # fix N+1
+    else
+      flash[:danger] = "User not found"
+      redirect_to root_url
+    end
   end
 
   def user_params
