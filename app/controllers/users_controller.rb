bb38ffab6ebf8f544984cde7a7c83bc0abe16b06
tranvanchilong <tranvanchilong@gmail.com>
2022-04-14 16:55:25 +0700

    profile_achiviement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:index, :edit, :update, :destroy, ]
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :show]
   def new
     @user = User.new
