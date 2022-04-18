7567e88cb28ce14ad3225c5e12e9386232851750
minhpham <minhpham161895@gmail.com>
2022-04-18 17:29:05 +0700

    list exam - time

--- app/controllers/user/exams_controller.rb
+++ app/controllers/user/exams_controller.rb
@@ -12,6 +12,7 @@ class User::ExamsController < ApplicationController
   end
 
   def show
+    params[:exam_time]
     @questions = @exam.questions.order_by_content
     @answers = Answer.includes(:question).where(question_id: @questions.ids).order_by_content
   end
