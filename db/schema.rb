8852b73a7863183fbecb2ddabc6ee4faac4a54af
tranvanchilong <tranvanchilong@gmail.com>
2022-04-18 12:57:55 +0700

    exam_cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_04_19_091005) do
+=======
+ActiveRecord::Schema.define(version: 2022_04_17_202552) do
+>>>>>>> exam_cart
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_04_19_091005) do
     t.integer "pass_score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_exam"
   end
 
   create_table "images", force: :cascade do |t|
