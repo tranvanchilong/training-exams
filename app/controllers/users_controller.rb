bb5799ffdb08cb0a50d9d395eb4c95d10e187a0f
tranvanchilong <tranvanchilong@gmail.com>
2022-04-23 20:09:00 +0700

    exam_cart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :selecting]
+  before_action :log_in, only: [:create]
   before_action :correct_user, only: [:edit, :update, :show, :selecting]
 
   def new
