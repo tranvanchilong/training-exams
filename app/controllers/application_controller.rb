78e7b58942cc632ddd1774f7addea65b04f7ea57
tranvanchilong <tranvanchilong@gmail.com>
2022-04-26 10:23:45 +0700

    show_exam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
    def correct_user
     @user = User.find_by(id: params[:id])
-    @exam = Exam.find_by(id: params[:id])
+    @exam = Exam.first
     redirect_to(root_url) unless current_user?(@user)
   end
 end
