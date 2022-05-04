1d8e34c6189db3b5bdb19c7266d743c8b8294a49
tranvanchilong <tranvanchilong@gmail.com>
2022-05-04 10:50:52 +0700

    profile_achiviement

--- app/helpers/achievement_helper.rb
+++ app/helpers/achievement_helper.rb
@@ -1,9 +1,9 @@
 module AchievementHelper
-    def is_passed user_exam
-        if user_exam.is_passed == true
-            "Passed"
-        else
-            "Fail"
-        end
+  def is_passed user_exam
+    if user_exam.is_passed == true
+      "Passed"
+    else
+      "Fail"
     end
+  end
 end
